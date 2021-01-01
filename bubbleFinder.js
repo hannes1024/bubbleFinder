7e79ff2e6b4357c2e3528781a74927c249e1ba79
v49r4n7 <hannes.hallstrom@gmail.com>
2021-01-01 17:05:06 +0100

    v 1.0

--- bubbleFinder.js
+++ bubbleFinder.js
@@ -1,5 +1,14 @@
 "use strict"
 let fileInput = document.getElementById("saveFile");
+let out = document.getElementById("output");
+let locationSelect = document.getElementById("locationSelect");
+let dayInput = document.getElementById("dayInput");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let seed = false;
+let day = false;
+let selectedLocation = false;
+let selectedLocationImg = false;
 fileInput.addEventListener("change", () => {
     if ("files" in fileInput) {
         const fileReader = new FileReader();
@@ -8,17 +17,71 @@ fileInput.addEventListener("change", () => {
 
             let daysPlayedStartTagIndex = contents.indexOf("<daysPlayed>") + 12;
             let daysPlayedEndTagIndex = contents.indexOf("</daysPlayed>", daysPlayedStartTagIndex);
-            let daysPlayed = parseInt(contents.substring(daysPlayedStartTagIndex, daysPlayedEndTagIndex));
+            setDay(parseInt(contents.substring(daysPlayedStartTagIndex, daysPlayedEndTagIndex)));
 
             let seedStartTagIndex = contents.lastIndexOf("<uniqueIDForThisGame>") + 21;
             let seedEndTagIndex = contents.indexOf("</uniqueIDForThisGame>", seedStartTagIndex);
-            let seed = parseInt(contents.substring(seedStartTagIndex, seedEndTagIndex));
-            
-            console.log(findBubbles(seed, daysPlayed, Forest));
+            seed = parseInt(contents.substring(seedStartTagIndex, seedEndTagIndex));
+
+            if (daysPlayedStartTagIndex === -1 || seedStartTagIndex == -1) {
+                alert("Failed to read nr of days played or seed from save file")
+            }
+
+            setLocation(locationSelect.value);
+            doIt();
         }
         fileReader.readAsText(fileInput.files[0]);
     }
 });
+
+function setDay(newDay) {
+    if (typeof newDay === "number" && Math.floor(newDay) === newDay && newDay > 0) {
+        day = newDay;
+        dayInput.value = newDay;
+    }
+}
+
+function setLocation (newLocation) {
+    if (newLocation in Locations) {
+        selectedLocation = newLocation;
+        selectedLocationImg = document.getElementById(`${selectedLocation}Img`);
+    }
+}
+
+locationSelect.addEventListener("change", () => {
+    setLocation(locationSelect.value);
+    doIt();
+})
+
+dayInput.addEventListener("change", () => {
+    setDay(parseInt(dayInput.value));
+    doIt();
+})
+
+function doIt() {
+    if (!seed || !day || typeof seed !== "number" || typeof day !== "number" || selectedLocation in Locations === false || !selectedLocation || !selectedLocationImg) return;
+    out.innerHTML = "";
+    canvas.setAttribute("width", 4 * Locations[selectedLocation].width);
+    canvas.setAttribute("height", 4 * Locations[selectedLocation].height);
+    ctx.drawImage(selectedLocationImg, 0, 0);
+
+    let bubbleArray = findBubbles(seed, day, Locations[selectedLocation]);
+    let listHTML = document.createElement("ol");
+    for (let i = 0; i < bubbleArray.length; i++) {
+        const bubble = bubbleArray[i];
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(`${bubble.start} to ${bubble.end} (${bubble.h}h ${bubble.m}m) at (${bubble.x}, ${bubble.y})`));
+        li.addEventListener("mouseover", () => {
+            ctx.lineWidth = 1;
+            ctx.strokeRect(4 * bubble.x, 4 * bubble.y, 4, 4);
+        })
+        li.addEventListener("mouseout", () => {
+            ctx.drawImage(selectedLocationImg, 0, 0);
+        })
+        listHTML.appendChild(li);
+    }
+    out.appendChild(listHTML);
+}
 
 /**
  * A function that returns an array with all bubbles on a given day in a given location.
@@ -39,11 +102,12 @@ function findBubbles(seed, day, location) {
                 let x = r.Next(0, location.width);
                 let y = r.Next(0, location.height);
                 if (location.tiles[y][x] === 1) {
-                    bubbles = {x, y, "start": time, "h": 0, "m": 0};
+                    bubbles = { x, y, "start": time, "h": 0, "m": 0, "duration": 0 };
                     break;
                 }
             }
         } else if (bubbles !== false) {
+            bubbles.duration++;
             bubbles.m += 10;
             if (bubbles.m == 60) {
                 bubbles.h++;
